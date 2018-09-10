41cb36d9e43babb9c6da5131ccea6b68a85fa47a
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-10 17:34:08 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,14 @@ end
 def array_with_two_elements
   array = [1,2]
 end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[2]
+end
+ def last_element(array)
+  array.last
+end
